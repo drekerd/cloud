732a9cae86004219266e97afd267e8c54a51c5d6
Mario Silva <mario.silva@jumia.com>
2021-01-16 22:50:00 +0000

    Controller

--- cloudBuild.sh
+++ cloudBuild.sh
@@ -10,13 +10,75 @@ remove_docker_image() {
   docker image rm -f ${PROJECT_NAME}
 }
 
+up_containers() {
+  docker-compose -f ${DOCKER_COMPOSE_FILE} --project-name ${DOCKER_PROJECT_NAME} up -d
+}
+
+down_containers() {
+  docker-compose -f ${DOCKER_COMPOSE_FILE} down --remove-orphans
+}
+
+build() {
+  ./gradlew build
+}
+
+clean() {
+  ./gradlew clean
+}
+
+unit_tests() {
+  ./gradlew test
+}
+
+acceptance_tests() {
+  ./gradlew acceptanceTests
+}
+
+exec_commands() {
+  for var in $@; do
+    case "$var" in
+    createDockerImage)
+      create_docker_image
+      ;;
+    removeDockerImage)
+      remove_docker_image
+      ;;
+    upContainers)
+      up_containers
+      ;;
+    downContainers)
+      down_containers
+      ;;
+    unitTests)
+      unit_tests
+      ;;
+    acceptanceTests)
+      acceptance_tests
+      ;;
+    esac
+  done
+}
+
 case $1 in
-createDockerImage)
-  create_docker_image
+-e)
+  exec_commands $@
   ;;
-removeDockerImage)
-  remove_docker_image
+*)
+
+  cat <<-EOF
+createDockerImage
+
+removeDockerImage
+
+upContainers
+
+downContainers
+
+unitTests
+
+acceptanceTests
+
+EOF
   ;;
-*) ;;
 
 esac
